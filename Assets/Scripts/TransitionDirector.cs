da60c44b9d1a8390b81fc9d6ecae4cd2ca4f650e
CutebatDev <alexaverten@gmail.com>
2025-01-24 15:02:29 +0200

    Transition Out + minor fix

--- Assets/Scripts/TransitionDirector.cs
+++ Assets/Scripts/TransitionDirector.cs
@@ -6,7 +6,7 @@ using UnityEngine.Playables;
 using System.Collections;
 using Unity.Cinemachine;
 using UnityEngine.Rendering.Universal;
-
+using UnityEngine.Timeline;
 
 namespace UHG
 {
@@ -16,33 +16,43 @@ namespace UHG
         [SerializeField] private GameController gameController;
         [SerializeField] private CinemachineCamera eyepeekCamera;
         [SerializeField] private CinemachineCamera camera2D;
-
-        [SerializeField] private PlayableDirector _playableDirector;
+        [SerializeField] private PlayableDirector playableDirector;
+        [SerializeField] private TimelineAsset timelineIn;
+        [SerializeField] private TimelineAsset timelineOut;
+        
         
         public void activateEyepeekCamera()
         {
             eyepeekCamera.Priority = 100;
             gameController.disablePlayerControls();
-            _playableDirector.Play();
-            // StartCoroutine(TransitionInCoroutine());
+            playableDirector.Play();
         }
-
-        // public IEnumerator TransitionInCoroutine()
-        // {
-        //     _playableDirector.Play();
-        //     while (_playableDirector.state == PlayState.Playing)
-        //     {
-        //         yield return null;
-        //     }
-        // }
         public void changeTo2D()
         {
+            playableDirector.playableAsset = timelineIn;
             Debug.Log("change to 2D");
             Camera.main.GetComponent<UniversalAdditionalCameraData>().SetRenderer(1);
             eyepeekCamera.Priority = -10;
             camera2D.Priority = 100;
+            StartCoroutine(waitAndTransitionOut());
+            
         }
-
+        public void changeTo3D()
+        {
+            playableDirector.playableAsset = timelineOut;
+            Debug.Log("change to 3D");
+            camera2D.Priority = -100;
+            eyepeekCamera.Priority = -10;
+            Camera.main.GetComponent<UniversalAdditionalCameraData>().SetRenderer(0);
+            gameController.disablePlayerControls();
+        }
+        
+        public IEnumerator waitAndTransitionOut() // DEBUG | CHANGE TO 2D EXIT FUNC
+        {
+            yield return new WaitForSeconds(2);
+            changeTo3D();
+            playableDirector.Play();
+        }
 
     }
 }
