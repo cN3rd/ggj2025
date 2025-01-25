243b9a98ec2f8b77d0ee5d5f7557b9d6414ac12f
cN3rd <subs.in.tokyo@gmail.com>
2025-01-25 11:44:27 +0200

    Add newspaper changer

---
+++ Assets/Scripts/NewspaperChanger.cs
@@ -0,0 +1,35 @@
+using System;
+using TMPro;
+using UHG;
+using UnityEngine;
+
+public class NewspaperChanger : MonoBehaviour
+{
+    [SerializeField] private CharacterData currentCharacter;
+    [SerializeField] private bool isGood;
+    
+    [SerializeField] private TextMeshPro mainTitleText;
+    [SerializeField] private TextMeshPro subTitleText;
+    [SerializeField] private SpriteRenderer characterSprite;
+
+    public void ChangeCurrentCharacter(CharacterData newCharacter)
+    {
+        currentCharacter = newCharacter;
+        if (isGood)
+        {
+            mainTitleText.text = newCharacter.mainTitleGood;
+            subTitleText.text = newCharacter.subTitleGood;
+            characterSprite.sprite = currentCharacter.paperGood;
+        }
+        else
+        {
+            mainTitleText.text = newCharacter.mainTitleBad;
+            subTitleText.text = newCharacter.subTitleBad;
+            characterSprite.sprite = currentCharacter.paperBad;
+        }
+    }
+
+    #if UNITY_EDITOR
+    private void OnValidate() => ChangeCurrentCharacter(currentCharacter);
+    #endif
+}
